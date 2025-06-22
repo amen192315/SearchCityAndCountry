be498197c221d250d0f4977e2bb121cd56bb8462
Vladislav <amen192315@gmail.com>
2025-06-22 19:25:39 +0300

    commit

--- src/app/pages/cities/components/city-details-popup/city-details-popup.component.ts
+++ src/app/pages/cities/components/city-details-popup/city-details-popup.component.ts
@@ -13,10 +13,11 @@ import {
 import { CustomNumber } from '../../../../core/pipes/custom-number.pipe';
 import { RoundPipe } from '../../../../core/pipes/round-number.pipe';
 import { CityData } from '../../models/cityData.interface';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-city-details-popup',
-  imports: [CustomNumber, MatDialogModule, RoundPipe],
+  imports: [CustomNumber, MatDialogModule, RoundPipe, TranslocoDirective],
   templateUrl: './city-details-popup.component.html',
   styleUrl: './city-details-popup.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/cities/components/city-edit-popup/city-edit-popup.component.ts
+++ src/app/pages/cities/components/city-edit-popup/city-edit-popup.component.ts
@@ -11,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CityData } from '../../models/cityData.interface';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-city-edit-popup',
@@ -21,6 +22,7 @@ import { CityData } from '../../models/cityData.interface';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    TranslocoDirective,
   ],
   templateUrl: './city-edit-popup.component.html',
   styleUrl: './city-edit-popup.component.scss',
@@ -32,18 +34,41 @@ export class CityEditPopupComponent {
   readonly data = inject<CityData>(MAT_DIALOG_DATA);
 
   editForm = this.fb.group({
-    name: [this.data.name, Validators.required],
-    country: [this.data.country, Validators.required],
-    region: [this.data.region, Validators.required],
+    name: [
+      this.data.name,
+      [Validators.required, Validators.pattern(/^[A-Za-z\s\-]+$/)],
+    ],
+    country: [
+      this.data.country,
+      [Validators.required, Validators.pattern(/^[A-Za-z\s\-]+$/)],
+    ],
+    region: [
+      this.data.region,
+      [Validators.required, Validators.pattern(/^[A-Za-z\s\-]+$/)],
+    ],
     population: [
       this.data.population,
-      [Validators.required, Validators.min(0)],
+      [Validators.required, Validators.min(0), Validators.pattern(/^\d+$/)],
     ],
-    latitude: [this.data.latitude],
-    longitude: [this.data.longitude],
-    regionCode: [this.data.regionCode],
+    latitude: [
+      this.data.latitude,
+      [Validators.required, Validators.min(-90), Validators.max(90)],
+    ],
+    longitude: [
+      this.data.longitude,
+      [Validators.required, Validators.min(-180), Validators.max(180)],
+    ],
   });
 
+  //метод для проверки валидности ф-мы
+  isInvalid(inp: string): boolean {
+    const control = this.editForm.get(inp);
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
+  }
+
+  //сохранение ф-мы
   saveChanges() {
     if (this.editForm.valid) {
       this.dialogRef.close(this.editForm.value);
